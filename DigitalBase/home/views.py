56da82ba4f5dac5ece65b4d5103f3ae5aace8e89
YChen89 <Yi.Chen@arup.com>
2021-11-26 18:47:10 -0500

    Work on basic setup of home app
    - include home.urls in project urls.py
    - create and write on urls.py for home app
    - add index function in views.py for home app
    - add home app in INSTALLED_APPS in project settings.py
    - create templates folder for home app
      - create layout.html for the whole project
        - include Bootstrap doc and a simple Navbar
      - create home folder
        - create index.html for home app
          - render simple order list for sample apps: Project, Task, Calc,
          Tool, Data
      - create static folder in root folder to store image, css, js for the project
        - create image, css, js folders
        - add Arup favicon in image folder
        - create empty styles.css file in css folder
        *Django static file management setup NOT working
    
    *Home app index page is functional. It displays a order list view of 5
    sample apps as # anchors.

--- DigitalBase/home/views.py
+++ DigitalBase/home/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
+
 from django.shortcuts import render
 
-# Create your views here.
+# All apps
+APPS = ['project',
+        'task',
+        'calc',
+        'tool',
+        'data']
+
+def index(request):
+    context = {'apps': APPS}
+    return render(request, 'home\index.html', context)
