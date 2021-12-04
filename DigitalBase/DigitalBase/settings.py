8beeb8fed91a36c2371ead093e697a259077b494
YChen89 <Yi.Chen@arup.com>
2021-12-04 16:50:42 -0500

    Work on basic setup of project app
    - include project.urls in project urls.py
    - copy urls.py from home app for project app and modify
    - add index function in views.py for project app
    - add project app in INSTALLED_APPS in project settings.py
    - create templates folder for project app
      - create project folder
        - create index.html for project app
          - use simple two-panel-layout.html template
    
    *Project app index page is functional. It displays two panels. The app
    is in a new branch called project.

--- DigitalBase/DigitalBase/settings.py
+++ DigitalBase/DigitalBase/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'home.apps.HomeConfig',
+    'project.apps.ProjectConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
