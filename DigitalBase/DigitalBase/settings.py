28be225f5bbc50c66975e89249aa136d3188ba30
YChen89 <Yi.Chen@arup.com>
2022-09-15 23:03:26 -0400

    Work on basic setup of calc app - include calc.urls in project urls.py - copy urls.py from home app for calc app and modify - add index function in views.py for calc app with simple layout - add calc app in INSTALLED_APPS in project settings.py - create templates folder for calc app   - create calc folder     - copy index.html from project app and modify       - keep two-panel page layout       - remove contents on the panels
    *Calc app index page is functional. It displays two panels. The app is
    in a new branch called calc.

--- DigitalBase/DigitalBase/settings.py
+++ DigitalBase/DigitalBase/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'home.apps.HomeConfig',
     'project.apps.ProjectConfig',
+    'calc.apps.CalcConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
