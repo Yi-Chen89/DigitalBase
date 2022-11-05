9c3466bc51e3c0963d0bd749206a0abd90fb4e1f
YChen89 <Yi.Chen@arup.com>
2022-11-05 12:09:22 -0400

    Work on Angular frontend home component - code and style home component in main section   - code layout in home.component.html   - add apps array in home.component.ts   - add style in styles.css
    *home page is completed at this stage

--- frontend/DigitalBase/src/app/home/home.component.ts
+++ frontend/DigitalBase/src/app/home/home.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  // apps data should come from the server
+  apps = [
+    'project',
+    'calc'
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
