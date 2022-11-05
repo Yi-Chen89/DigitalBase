bd67a8be6778821cccb3ce0f3682a49e884decef
YChen89 <Yi.Chen@arup.com>
2022-11-05 12:51:07 -0400

    Work on Angular frontend calc component - create and style calc component in main section   - generate calc component "ng g c calc"   - code layout in calc.component.html   - set up route for calc component in app-routing.module.ts
    *calc page is completed with no content

--- frontend/DigitalBase/src/app/app-routing.module.ts
+++ frontend/DigitalBase/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { ProjectComponent } from './project/project.component';
+import { CalcComponent } from './calc/calc.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'project', component: ProjectComponent },
+  { path: 'calc', component: CalcComponent },
 ];
 
 @NgModule({
--- frontend/DigitalBase/src/app/app.module.ts
+++ frontend/DigitalBase/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { ProjectComponent } from './project/project.component';
+import { CalcComponent } from './calc/calc.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ProjectComponent } from './project/project.component';
     HeaderComponent,
     FooterComponent,
     HomeComponent,
-    ProjectComponent
+    ProjectComponent,
+    CalcComponent
   ],
   imports: [
     BrowserModule,
